d0d58a9292421fc75b0d354b26244f381f6d5d07
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-10-07 13:33:37 -0400

    date time stamp added but pop-ups blew apart

--- main.js
+++ main.js
@@ -1,32 +1,74 @@
 var subTotal = 0;
-
+var merchandise = "";
 var blueCartButton = document.getElementById("buyBlue");
 var costBox = document.getElementById("cost");
-var isBlueInCart = false
 var withTax = document.getElementById("total");
+var cartContents = document.getElementById("cart");
+var today = document.getElementById("demo");
+var cart = [];
 
-
-function updateCart (){
-	var total = subTotal * .065 + subTotal;
-	costBox.innerHTML = subTotal;
-	withTax.innerHTML = total;
+var blueShirt = {
+    color: "blue",
+    price: 20,
+    name: "Save My Trees",
+    inCart: false,
+    date: "",
 }
 
-blueCartButton.addEventListener("click", function(){
+var pinkShirt = {
+    color: "pink",
+    price: 25,
+    name: "Nature Lover",
+    inCart: false,
+    date: "",
+}
 
-	if (isBlueInCart === false){
-		subTotal = subTotal + 20;
-		updateCart();
-		isBlueInCart = true;
-		blueCartButton.className = "active icon icon5";
+var orangeShirt = {
+    color: "orange",
+    price: 30,
+    name: "Forest Walk",
+    inCart: false,
+    date: "",
+}
 
-	}
-	else {
-		subTotal = subTotal - 20;
-		updateCart();
-		isBlueInCart = false;
-		blueCartButton.className = "icon icon5";
-	}
+function updateCart() {
+    cartContents.innerHTML = "";
+    merchandise = "";
+
+    cart.forEach(function(shirt) {
+        merchandise += `
+	  <div class="item">
+	  <span class="shirtName">${shirt.name}</span>
+	  <span class="shirtPrice">$ ${shirt.price.toFixed(2)}</span>
+	  <span class="dateStamp">${shirt.date}</span>
+	   </div>
+	  `;
+    });
+    cartContents.innerHTML = merchandise;
+    var total = subTotal * .065 + subTotal;
+    costBox.innerHTML = subTotal.toFixed(2);
+    withTax.innerHTML = total.toFixed(2);
+}
+
+
+blueCartButton.addEventListener("click", function() {
+
+    if (blueShirt.inCart === false) {
+        subTotal = subTotal + blueShirt.price;
+        blueShirt.inCart = true;
+        blueCartButton.className = "active icon icon5";
+        blueShirt.date = Date();
+        cart.push(blueShirt);
+        updateCart();
+
+    } else {
+        subTotal = subTotal - blueShirt.price;
+        blueShirt.inCart = false;
+        blueCartButton.className = "icon icon5";
+        var location = cart.indexOf(blueShirt);
+        cart.splice(location, 1);
+        updateCart();
+    }
 });
 
 
@@ -35,20 +77,22 @@ var pinkCartButton = document.getElementById("buyPink");
 var costBox = document.getElementById("cost");
 var isPinkInCart = false
 
-pinkCartButton.addEventListener("click", function(){
+pinkCartButton.addEventListener("click", function() {
 
-	if (isPinkInCart === false){
-		subTotal = subTotal + 25;
-		updateCart();
-		isPinkInCart = true;
-		pinkCartButton.className = "active icon icon5";
-	}
-	else {
-		subTotal = subTotal - 25;
-		updateCart();
-		isPinkInCart = false;
-		pinkCartButton.className = "icon icon5";
-	}
+    if (isPinkInCart === false) {
+        subTotal = subTotal + 25;
+        isPinkInCart = true;
+        pinkCartButton.className = "active icon icon5";
+        pinkShirt.date = Date();
+        cart.push(pinkShirt);
+        updateCart();
+    } else {
+        subTotal = subTotal - 25;
+        isPinkInCart = false;
+        pinkCartButton.className = "icon icon5";
+        cart.splice(location, 1);
+        updateCart();
+    }
 });
 
 
@@ -57,19 +101,20 @@ var orangeCartButton = document.getElementById("buyOrange");
 var costBox = document.getElementById("cost");
 var isOrangeInCart = false
 
-orangeCartButton.addEventListener("click", function(){
+orangeCartButton.addEventListener("click", function() {
 
-	if (isOrangeInCart === false){
-		subTotal = subTotal + 30;
-		updateCart();
-		isOrangeInCart = true;
-		orangeCartButton.className = "active icon icon5";
-	}
-	else {
-		subTotal = subTotal - 30;
-		updateCart();
-		isOrangeInCart = false;
-		orangeCartButton.className = "icon icon5"
-	}
+    if (isOrangeInCart === false) {
+        subTotal = subTotal + 30;
+        isOrangeInCart = true;
+        orangeCartButton.className = "active icon icon5";
+        orangeShirt.date = Date();
+        cart.push(orangeShirt);
+        updateCart();
+    } else {
+        subTotal = subTotal - 30;
+        isOrangeInCart = false;
+        orangeCartButton.className = "icon icon5"
+        cart.splice(location, 1);
+        updateCart();
+    }
 });
-
